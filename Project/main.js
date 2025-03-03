2c50e806b5ca5e68c01382eb4845881954a444dd
TeddyTez <teddutez123@gmail.com>
2025-03-03 14:22:29 +0300

    initial commit

---
+++ Project/main.js
@@ -0,0 +1,8 @@
+const change = document.getElementById("content");
+
+const changeContent = () => {
+  let newContent =
+    "It is a distributed version control system, which means that your local copy of the code is a complete repository.";
+
+  change.textContent = newContent;
+};
